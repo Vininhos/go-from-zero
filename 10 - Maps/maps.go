104e5f061ac6888b370a230ffe33a38668290e55
Vininhos <viniciusrw2011@hotmail.com>
2023-03-19 00:37:27 -0300

    Criado aplicação de linha de comando

--- 10 - Maps/maps.go
+++ 10 - Maps/maps.go
@@ -1,22 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 	fmt.Println("Maps")
 
-	usuario := map[string]string {
-		"nome" : "Pedro",
-		"sobrenome": "Silva"
+	usuario := map[string]string{
+		"nome":      "Pedro",
+		"sobrenome": "Silva",
 	}
 
 	fmt.Println(usuario)
 
-	usuario2 := map[string]map[string]string {
+	usuario2 := map[string]map[string]string{
 		"nome": {
-			"primeiro" : "João",
-			"ultimo": "Pedro"
+			"primeiro": "João",
+			"ultimo":   "Pedro",
 		},
 		"curso": {
-			"nome": "Engenharia",
+			"nome":   "Engenharia",
 			"campus": "Campus 1",
 		},
 	}
@@ -25,7 +27,7 @@ func main() {
 	delete(usuario2, "nome")
 	fmt.Println(usuario2)
 
-	usuario2["signo"] = map[string]string {
+	usuario2["signo"] = map[string]string{
 		"nome": "Gêmeos",
 	}
 
